bc572548bcc82e3f22d9bffee4b675e1699e7870
askmuhsin <askmuhsin@gmail.com>
2018-07-16 07:19:52 +0800

    code refactor

--- get_data.py
+++ get_data.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 import os
 
-loc_src = "/home/muhsin/code/master_thes/prepare_data/data/img_preprocessed_set_1/"
+loc_src_1 = "/home/muhsin/code/master_thes/human_activity_sdha/data/img_preprocessed_set_1/"
+loc_src_2 = "/home/muhsin/code/master_thes/human_activity_sdha/data/img_preprocessed_set_2/"
 
 def getClassNum(video_file_name):
     """does some string operations to obtain class num"""
@@ -10,24 +11,30 @@ def getClassNum(video_file_name):
     class_of_file = int(file_parts[0])
     return class_of_file
 
-def readImg(file_name, loc_src=loc_src):
+def readImg(file_name, loc_src):
     file_loc = loc_src + file_name
     img = cv2.imread(file_loc)
     img = cv2.resize(img, (32, 32))
     return img
 
 def load_data():
-    file_names = os.listdir(loc_src)
     X, y = [], []
+    file_names = os.listdir(loc_src_1)
     for file in file_names:
         y.append(getClassNum(file))
-        img = readImg(file)
+        img = readImg(file, loc_src_1)
         X.append(img[:,:,0])
+
+    file_names = os.listdir(loc_src_2)
+    for file in file_names:
+        y.append(getClassNum(file))
+        img = readImg(file, loc_src_2)
+        X.append(img[:,:,0])
+
     return X, y
 
 def main():
-    # X, y = load_data()
-    pass
+    X, y = load_data()
 
 if __name__ == '__main__':
     main()
