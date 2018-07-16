3522c8fc841eee9ed9bc4261ff416ea0abd1bae1
askmuhsin <askmuhsin@gmail.com>
2018-07-17 05:07:32 +0800

    initial test done

---
+++ sequence_generator.py
@@ -0,0 +1,134 @@
+from tensorflow.contrib.layers import flatten
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from get_data import load_data
+import numpy as np
+import random
+import cv2
+import sys
+import os
+
+loc_src_1 = "./data/img_preprocessed_set_1/"
+loc_src_2 = "./data/img_preprocessed_set_2/"
+
+class_nums = 6
+class_bins = {0: 'shake_hands',
+             1: 'hug',
+             2: 'kick',
+             3: 'point',
+             4: 'punch',
+             5: 'push'}
+
+# classNum = lambda file_name : fileName.split('_')[0]
+
+
+
+def readImg(file_name):
+    img = cv2.imread(file_name)
+    img = cv2.resize(img, (32, 32))
+    return img
+
+def LeNetSequenceGen(x):
+    # Hyperparameters
+    mu = 0
+    sigma = 0.1
+    layer_depth = {
+    'layer_1' : 6,
+    'layer_2' : 16,
+    'layer_3' : 120,
+    'layer_f1' : 84
+    }
+
+    ## Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+    conv1_w = tf.Variable(tf.truncated_normal(shape = [5,5,1,6],mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1 = tf.nn.conv2d(x,conv1_w, strides = [1,1,1,1], padding = 'VALID') + conv1_b
+    ## Activation.
+    conv1 = tf.nn.relu(conv1)
+
+    ## Pooling. Input = 28x28x6. Output = 14x14x6.
+    pool_1 = tf.nn.max_pool(conv1,ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'VALID')
+
+    ## Layer 2: Convolutional. Output = 10x10x16.
+    conv2_w = tf.Variable(tf.truncated_normal(shape = [5,5,6,16], mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2 = tf.nn.conv2d(pool_1, conv2_w, strides = [1,1,1,1], padding = 'VALID') + conv2_b
+    ## Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    ## Pooling. Input = 10x10x16. Output = 5x5x16.
+    pool_2 = tf.nn.max_pool(conv2, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'VALID')
+
+    ## Flatten. Input = 5x5x16. Output = 400.
+    fc1 = flatten(pool_2)
+
+    ## Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_w = tf.Variable(tf.truncated_normal(shape = (400,120), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1 = tf.matmul(fc1,fc1_w) + fc1_b
+
+    # cache sequence
+    seq_fc1 = fc1
+
+    ## Activation.
+    fc1 = tf.nn.relu(fc1)
+
+    ## Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_w = tf.Variable(tf.truncated_normal(shape = (120,84), mean = mu, stddev = sigma))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1,fc2_w) + fc2_b
+    ## Activation.
+    fc2 = tf.nn.relu(fc2)
+
+    ## Layer 5: Fully Connected. Input = 84. Output = 10.
+    fc3_w = tf.Variable(tf.truncated_normal(shape = (84, class_nums), mean = mu , stddev = sigma))
+    fc3_b = tf.Variable(tf.zeros(class_nums))
+    logits = tf.matmul(fc2, fc3_w) + fc3_b
+
+    return logits, seq_fc1
+
+def closureClassName(num):
+    # filter_class = closureClassName(0)
+    # file_names = os.listdir(loc_src_1)
+    # file_names_0 = filter(filter_class, file_names)
+    def filterByClass(file):
+        if class_num(file)==num:
+            return True
+        else:
+            return False
+            return filterByClass
+
+def get_x():
+    c_n , s_n , f_n = 6, 10, 1000
+    for class_num in range(0, c_n):
+        for seq_num in range(1, s_n):
+            for frame_num in range(0, f_n):
+                file_name = loc_src_1 + f"{class_num}_{seq_num}_{frame_num}.png"
+                if os.path.isfile(file_name):
+                    img = readImg(file_name)
+                    img = img[:,:,0]
+                    img = ((img-255)/255)
+                    test_img = img
+                    test_img = test_img.reshape(1, 32, 32, 1)
+                    yield test_img, [class_num, seq_num, frame_num]
+
+def main():
+    x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+    logits = LeNetSequenceGen(x)
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        for item in get_x():
+            saver.restore(sess, tf.train.latest_checkpoint('./model/'))
+            output, seq = sess.run(logits, feed_dict={x: item[0]})
+            print(seq[0])
+            print()
+            print()
+            # print(output)
+            # print()
+            # print(f'Model prediction -> {class_bins[np.argmax(output)]}, ({np.argmax(output)})')
+            # print(type(seq[0]))
+            # print(seq[0].shape)
+
+if __name__ == '__main__':
+    main()
